9630a4dbb8943ba955e4105e302e5d3bc277154a
haeke <riverae2@student.wpunj.edu>
2019-06-10 17:13:58 -0400

    intitial project commit, there is currently a main router and a persons router, the main router will return a hard coded notes json object and the persons router has a route to return a hard coded phonebook.

---
+++ index.js
@@ -0,0 +1,95 @@
+// A simple server that uses the HTTP library
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+// other routers that we want to use
+const persons = require("./routes/persons");
+
+app.use(bodyParser.json());
+
+// use the persons router
+app.use("/api/persons", persons);
+
+let notes = [
+  {
+    id: 1,
+    content: "HTML IS EASY",
+    date: "2019-05-30T17:30:31.098Z",
+    important: true
+  },
+  {
+    id: 2,
+    content: "Browser can only execute JavaScript",
+    date: "2019-05-30T17:20:31.098Z",
+    important: false
+  },
+  {
+    id: 3,
+    content: "GET and POST are the most important methods of HTTP protocol",
+    date: "2019-05-30T14:39:31.098Z",
+    important: true
+  }
+];
+// stand alone function that will return the maximum id from the list of notes
+const generateId = () => {
+  const maxId = notes.length > 0 ? Math.max(...notes.map(note => note.id)) : 0;
+  return maxId + 1;
+};
+
+app.post("/notes", (req, res) => {
+  const body = req.body;
+
+  // make sure the body has content
+  if (!body.content) {
+    return res.status(400).json({ error: "content missing" });
+  }
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+    id: generateId()
+  };
+  notes = notes.concat(note);
+  res.json(note);
+});
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello World!</h1>");
+});
+
+// define a route that will return a note by ID
+app.get("/note/:id", (req, res) => {
+  // req.params.id
+  // make sure that we convert the type of the id passed to the router from type String to type Number
+  const id = Number(req.params.id);
+  console.log("id : ", id);
+  let note = notes.find(note => note.id === id);
+  console.log("note value: ", note);
+  if (note) {
+    console.log("sending note...");
+    res.json(note);
+  } else {
+    res.send(404).end();
+  }
+});
+
+// define a route that will delete a resource by id
+app.delete("/note/:id", (req, res) => {
+  const id = Number(req.params.id);
+
+  notes = notes.filter(note => note.id === id);
+
+  // status 204 no content will return a no data response
+  res.send(204).end();
+});
+
+app.get("/notes", (req, res) => {
+  res.json(notes);
+});
+
+const port = 3001;
+app.listen(port);
+
+console.log(`Server is running on http://localhost:${port}`);
---
+++ routes/persons.js
@@ -0,0 +1,56 @@
+const express = require("express");
+// Create a new router instance
+const app = express.Router();
+//@route /api/persons/test
+//@desc This route is just a test to make sure the router is working with the main router
+//@access public access
+app.get("/test", (req, res) => {
+  res.json({ working: "The phone-book route is working." });
+});
+
+const persons = [
+  {
+    name: "Manny Pacquiao",
+    number: "010-230957",
+    id: 1
+  },
+  {
+    name: "Floyd Mayweather",
+    number: "101-2309457",
+    id: 2
+  },
+  {
+    name: "Muhammad Ali",
+    number: "309-309482",
+    id: 3
+  },
+  {
+    name: "Mike Tyson",
+    number: "123-4828322",
+    id: 4
+  },
+  {
+    name: "Oscar De La Hoya",
+    number: "490-492379",
+    id: 5
+  },
+  {
+    name: "Gennady Golovkin",
+    number: "128-4848292",
+    id: 6
+  },
+  {
+    name: "Canelo Alvarez",
+    number: "321-324874621",
+    id: 7
+  }
+];
+
+//@route /api/persons
+//@desc Will return a hard coded list of phonenook entries
+//@access public
+app.get("/", (req, res) => {
+  res.json(persons);
+});
+
+module.exports = app;
